e97565628aa6e77e280a3b02a06d9e41a3a985ba
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-02 10:27:30 +0530

    Data configuration

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 import express from "express";
+import cors from "cors";
+import { DATA_LIMIT } from "./constants.js";
+import cookieParser from "cookie-parser";
 
-export const app = express();
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+);
+
+// Down below: Data Configuration
+app.use(express.json({ limit: DATA_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: DATA_LIMIT }));
+app.use(express.static("public"));
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET))
+
+
+
+export { app };
