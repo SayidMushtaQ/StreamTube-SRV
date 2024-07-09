9f7431ce98b7ab4e5bd2b30e89e48252c8f56ba2
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-09 18:08:12 +0530

    User Register - Validation and more

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,34 @@
 import { asyncHandler } from "../util/asyncHandler.util.js";
-import {apiResponse} from '../util/apiResponse.utils.js';
+import { apiResponse } from "../util/apiResponse.utils.js";
+import { ApiError } from "../util/apiError.utils.js";
+import { EMAIL_REGEX } from "../constants.js";
+import { User } from "../modules/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json(
-    new apiResponse(200,{Ok:'Ok'},'Data retrieved successfully')
-  )
+  const { userName, fullName, email, password } = req.body;
+  if (
+    [userName, fullName, email, password].some(field => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required", [
+      "Please fill up all necessary fields",
+      "Check userName,fullName,email and password fields"
+    ]);
+  }
+  if (!EMAIL_REGEX.test(email)) {
+    throw new ApiError(400, "Invalid email format");
+  }
+
+  const isAlreadyUserExist = await User.find({
+    $or: [{ userName }, { email }]
+  });
+  
+  if(isAlreadyUserExist){
+    throw new ApiError(409,'User already exists')
+  }
+
+  res
+    .status(200)
+    .json(new apiResponse(200, { Ok: "Ok" }, "Data retrieved successfully"));
 });
 
-
-export {registerUser};
+export { registerUser };
